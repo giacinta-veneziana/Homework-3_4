c5a23ad11ad48c76e2bcb0f3d65051ebdaffe333
Giacinta <arthur.s@greengreyholding.com>
2024-05-26 10:59:27 +0300

    Added function str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_func(some_string: str):
+    """returns uppercase string"""
     return some_string.upper()
+
+def another_str_func(some_string: str):
+    """returns capitalized string"""
+    return some_string.capitalize()
