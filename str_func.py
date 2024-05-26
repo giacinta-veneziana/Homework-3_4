eb39c3d82affaf39ae329ed78a758e9c244840fa
Alexandra Gorokhova <84781034+giacinta-veneziana@users.noreply.github.com>
2024-05-26 11:10:44 +0300

    Update str_func.py
    All bugs fixed. Definitely.

--- str_func.py
+++ str_func.py
@@ -5,3 +5,5 @@ def str_func(some_string: str):
 def another_str_func(some_string: str):
     """returns capitalized string"""
     return some_string.capitalize()
+
+#ALL BUGS FIXED!!!! NO BUGS HERE!!!!!!!!!!
